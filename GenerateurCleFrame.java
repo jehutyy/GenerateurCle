d73b1a0ab41186c11a53e38cdaf17b187af34334
alex <pagnat7@hotmail.com>
2014-12-18 19:04:20 +0100

    ajout JSpinner+SpinnerNumberModel

--- GenerateurCleFrame.java
+++ GenerateurCleFrame.java
@@ -6,16 +6,21 @@ import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
 import javax.swing.JTextArea;
+import javax.swing.SpinnerNumberModel;
 
 public class GenerateurCleFrame extends JFrame implements ActionListener {
 	private JTextArea area;
 	private JButton bouton;
 	private JCheckBox boxMaj;
 	private JCheckBox boxDigit;
+	private JSpinner rangePass;
+	private SpinnerNumberModel model;
 	private JList<String> list;
 	private Key keygen;
 
@@ -27,15 +32,18 @@ public class GenerateurCleFrame extends JFrame implements ActionListener {
 	public void build() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
-		this.setSize(200, 75);
+		this.setSize(220, 75);
 		this.setResizable(false);
 		this.setTitle("keygen");
 		setContentPane(buildPan());
 	}
 
 	public JPanel buildPan() {
+		/*JPanel maitre */
 		JPanel panel = new JPanel(new BorderLayout());
+		/*JPanel bouton generer */
 		JPanel button = new JPanel(new BorderLayout());
+		/*JPanel checkbox majuscule et chiffres et JSpinner */
 		JPanel checkbox = new JPanel(new BorderLayout());
 
 		bouton = new JButton("générer");
@@ -46,15 +54,14 @@ public class GenerateurCleFrame extends JFrame implements ActionListener {
 		boxMaj = new JCheckBox("Majuscules");
 		boxDigit = new JCheckBox("Chiffres");
 		
-		/*list = new JList<String>();
-		for (int i = 0; i < 13; i++) {
-			list.
-		}*/
-
+		model = new SpinnerNumberModel(0, 0, 100, 1);
+		rangePass = new JSpinner(model);
+		
 		button.add(bouton, BorderLayout.WEST);
 		button.add(area);
 		checkbox.add(boxMaj, BorderLayout.WEST);
-		checkbox.add(boxDigit, BorderLayout.EAST);
+		checkbox.add(boxDigit, BorderLayout.CENTER);
+		checkbox.add(rangePass, BorderLayout.EAST);
 
 		panel.add(button, BorderLayout.NORTH);
 		panel.add(checkbox, BorderLayout.SOUTH);
@@ -66,7 +73,7 @@ public class GenerateurCleFrame extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		Object o = e.getSource();
 		if (o == bouton) {
-			int i = 12;
+			int i = model.getNumber().intValue();
 			if (boxMaj.isSelected() && boxDigit.isSelected() == false) {
 				keygen = new Key(i, true, false);
 				area.setText(keygen.getCle());
